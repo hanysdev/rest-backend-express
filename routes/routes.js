22879a8d75f62a186d2c648426e08a5901bac309
hanysdev <damian.schygulla@gmail.com>
2023-04-23 00:29:11 +0200

    crud

--- routes/routes.js
+++ routes/routes.js
@@ -1,28 +1,70 @@
 const express = require('express');
 const router = express.Router()
+const Model = require('../model/model');
 module.exports = router;
 
 //Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(201).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+    try {
+        const dataToGet = await Model.find()
+        res.status(200).json(dataToGet)
+    } catch (error) {
+        res.status(500).json({message: error.message})    
+    }
 })
 
 //Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
+router.get('/getOne/:id', async (req, res) => {
+    try {
+        const dataToGet = await Model.findById(req.params.id)
+        res.status(200).json(dataToGet)
+    } catch (error) {
+        res.status(500).json({message: error.message})    
+    }
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send('Document with id:  ${data.name} has been deleted.')
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
