f0aa4376cca1751a863594dd743a77d3080186d0
hanysdev <damian.schygulla@gmail.com>
2023-05-23 04:00:53 +0200

    for prod

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 module.exports = router;
 
 const corsOptions = {
-    origin: 'http://localhost:4200',
+    origin: ['https://frontend-angular.herokuapp.com/', 'https://tsw-front-57m3.vercel.app', 'https://tsw-front.vercel.app/'],
     optionsSuccessStatus: 200
   };
   
